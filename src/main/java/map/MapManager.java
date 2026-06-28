2a4e5da334eb2dcc756520a11095700591cb1f5c
hieultce201492-gif <hieultce201492@gmail.com>
2026-06-28 19:57:02 +0700

    * đã thêm chọn map
    * hieult
    
    * đã thêm chọn map

--- src/main/java/map/MapManager.java
+++ src/main/java/map/MapManager.java
@@ -20,7 +20,7 @@ public class MapManager {
     public MapManager() {
         mapMatrix = new int[maxRow][maxCol];
 
-        loadMap("/maps/map01.txt");
+      //  loadMap("/maps/map01.txt");
     }
 
     public void loadMap(String filePath) {
