4a7f51f5fe82123777ca0776d9996423386c9f68
bomberman <hieultce201492@gmail.com>
2026-07-01 17:53:46 +0700

    thêm 1 vài hình ảnh sửa quay trái phải lên xuống ở panel và enemy

--- src/main/java/model/Enemy.java
+++ src/main/java/model/Enemy.java
@@ -10,7 +10,7 @@ public class Enemy extends GameObject {
     
     private int speed = 2; // Tốc độ di chuyển (Nên để số nguyên chẵn như 2 để khớp Grid 48)
     private int currentDir = -1; // Hướng hiện tại: 0=Lên, 1=Xuống, 2=Trái, 3=Phải
-    
+    public String direction = "right";
     private static final int TILE_SIZE = 48; 
     private int[][] currentMap;
     private Random random = new Random();
@@ -41,11 +41,14 @@ public class Enemy extends GameObject {
         }
 
         // Cứ thế tiếp tục bước đi theo hướng đã chọn
-        if (currentDir == 0) this.setY(this.Y - speed); // Lên
-        else if (currentDir == 1) this.setY(this.Y + speed); // Xuống
-        else if (currentDir == 2) this.setX(this.X - speed); // Trái
-        else if (currentDir == 3) this.setX(this.X + speed); // Phải
-
+        if (currentDir == 0) {this.setY(this.Y - speed); // Lên
+        direction = "up";  }        // THÊM DÒNG NÀY
+        else if (currentDir == 1) {this.setY(this.Y + speed); // Xuống
+                direction = "down"; }       // THÊM DÒNG NÀY
+        else if (currentDir == 2) {this.setX(this.X - speed); // Trái
+        direction = "left";}
+        else if (currentDir == 3) {this.setX(this.X + speed); // Phải
+         direction = "right";}
         return true; 
     }
 
@@ -57,10 +60,9 @@ public class Enemy extends GameObject {
 
         if (row > 0 && currentMap[row - 1][col] == 0) dirs.add(0); // Lên
         if (row < currentMap.length - 1 && currentMap[row + 1][col] == 0) dirs.add(1); // Xuống
-        if (col > 0 && currentMap[row][col - 1] == 0) dirs.add(2); // Trái
-        if (col < currentMap[0].length - 1 && currentMap[row][col + 1] == 0) dirs.add(3); // Phải
-        
-        return dirs;
+        if (col > 0 && currentMap[row][col - 1] == 0) dirs.add(2); // Trái  
+        if (col < currentMap[0].length - 1 && currentMap[row][col + 1] == 0) dirs.add(3); // Phải 
+       return dirs;
     }
 
     @Override
